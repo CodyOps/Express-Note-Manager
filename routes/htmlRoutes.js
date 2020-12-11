a6a4c3d9b32ae7223ed8c953a6d86371e4f012f5
Cody Chen <codychen1@gmail.com>
2020-12-11 12:49:42 -0700

    added to htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,21 @@
+//DEPENDENCIES
+//We need to include the path packages to get the correct file path for out html
+
+const path = require("path");
+
+//ROUTING
+
+module.exports = (app) => {
+  //HTML GET requests
+  //Below code handles when a user visits a page.
+  //In each of the cases below the user is shown an HTML page of the content
+
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  //if no matching route is found, default to homepage
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
